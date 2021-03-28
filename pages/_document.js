d09d19233a3cc398a12d8862068a6ed68af02484
Mattèo <mattix.contact@gmail.com>
2021-03-28 04:18:38 +0200

    imporving seo by adding title and description

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,22 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link type="application/rss+xml" rel="alternate" title="Le Monde à La Carte" href="https://anchor.fm/s/53ca1838/podcast/rss" />
+					<link
+						type="application/rss+xml"
+						rel="alternate"
+						title="Le Monde à La Carte"
+						href="https://anchor.fm/s/53ca1838/podcast/rss"
+					/>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-
+					<title>
+						Le Monde à La Carte - Podcast sur le voyage et la culture, découvrez votre planète sans changer d'hémisphère
+					</title>
+					<meta
+						name="description"
+						content="Découvrez votre planète sans changer d’hémisphère. Une émission culturelle créée par des étudiants
+						bordelais. Votre petite dose de culture générale."
+					/>
 					<script async src="https://www.googletagmanager.com/gtag/js?id=G-9R7K00797N"></script>
 					<script
 						dangerouslySetInnerHTML={{
