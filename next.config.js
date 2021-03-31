3833114d2437692cb4024745fca295972a09ca7b
Mattèo <mattix.contact@gmail.com>
2021-03-31 15:53:37 +0200

    Editing google podcast url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 		instagramLink: "https://instagram.com/lemondealacarte_",
 		deezerLink: "https://www.deezer.com/fr/show/2464042",
 
-		googlePodcastLink: "https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy81M2NhMTgzOC9wb2RjYXN0L3Jzcw==",
+		googlePodcastLink: "https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy81M2NhMTgzOC9wb2RjYXN0L3Jzcw",
 		applePodcastLink: "https://podcasts.apple.com/us/podcast/le-monde-%C3%A0-la-carte/id1560528157",
 	},
 };
